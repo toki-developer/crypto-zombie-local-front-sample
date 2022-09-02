a4b4eb80e160fa33c1ebe23f7afd65a27a9eebde
Toki <55911995+tokio-k@users.noreply.github.com>
2022-09-03 00:12:52 +0900

    Connect to contract from frontend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import Web3 from 'web3';
+import { AbiItem } from 'web3-utils';
 import type { AppProps } from 'next/app'
+import ZombieFactoryContract from "../../build-contracts/ZombieFactory.json"
+import { ZombieFactory } from '../types/abi';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const networkAddress = "http://127.0.0.1:7545"
+const web3 = new Web3(new Web3.providers.HttpProvider(networkAddress))
+const contractAddress = "0xeb3554DE5ba4f7a68F2aAa2e6C2f107C7dF3dd0b"
+const ABI = ZombieFactoryContract.abi as unknown  as AbiItem
+const contract = new web3.eth.Contract(ABI, contractAddress) as unknown as ZombieFactory
+
+
+function  MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
