16dc0c0057630aaff4ceee69bf3724f739c73761
Toki <55911995+tokio-k@users.noreply.github.com>
2022-09-09 13:22:02 +0900

    change file to connect contract

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import Web3 from 'web3';
+import { AbiItem } from 'web3-utils';
+import ZombieFactoryContract from "../../build-contracts/ZombieFactory.json"
+import { ZombieFactory } from '../types/abi';
+
+const networkAddress = "http://127.0.0.1:7545"
+const web3 = new Web3(new Web3.providers.HttpProvider(networkAddress))
+const contractAddress = "0xeb3554DE5ba4f7a68F2aAa2e6C2f107C7dF3dd0b"
+const ABI = ZombieFactoryContract.abi as unknown  as AbiItem
+const contract = new web3.eth.Contract(ABI, contractAddress) as unknown as ZombieFactory
 
 const Home: NextPage = () => {
+  
   return (
     <div>
     </div>
