cd2bff2b8a4ab7f7cfb6205b4ad3b140279e0d5f
Toki <55911995+tokio-k@users.noreply.github.com>
2022-09-13 10:44:27 +0900

    change contract info from sample to lesson 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
 import type { NextPage } from 'next'
 import Web3 from 'web3';
 import { AbiItem } from 'web3-utils';
-import ZombieFactoryContract from "../../build-contracts/ZombieFactory.json"
-import { ZombieFactory } from '../types/abi';
+import ZombieAttackContract from "../../build-contracts/ZombieAttack.json"
+import { ZombieAttack } from '../types/abi';
 
 const networkAddress = "http://127.0.0.1:7545"
 const web3 = new Web3(new Web3.providers.HttpProvider(networkAddress))
-const contractAddress = "0xeb3554DE5ba4f7a68F2aAa2e6C2f107C7dF3dd0b"
-const ABI = ZombieFactoryContract.abi as unknown  as AbiItem
-const contract = new web3.eth.Contract(ABI, contractAddress) as unknown as ZombieFactory
+const contractAddress = "0x028Cb081d8277C491Fd6F907891cC61b99e91302"
+const ABI = ZombieAttackContract.abi as unknown  as AbiItem
+const contract = new web3.eth.Contract(ABI, contractAddress) as unknown as ZombieAttack
 
 const Home: NextPage = () => {
-  
+
+  console.log(contract.methods.testAccessContract().call())
+
   return (
     <div>
     </div>
